d981c20c5a9a04a9effbbfd57945812f2b9410cf
Rodrigo P <rodpin123@hotmail.com>
2020-08-22 20:47:40 -0300

    First commit for this project

---
+++ FirstProject/Data/OODB/OfflineDB.bat
@@ -0,0 +1,4 @@
+rem OfflineDB.bat 3-31-2006
+rem OfflineDB - PLACE IN C:\UCOS\UCOS_PDE_Projects\PROJNAME\Data\OODB
+
+osdbcontrol -offline ucos.db
---
+++ FirstProject/Data/OODB/OnlineDB.bat
@@ -0,0 +1,4 @@
+rem OnlineDB.bat 3-31-2006
+rem OnlineDB - PLACE IN C:\UCOS\UCOS_PDE_Projects\PROJNAME\Data\OODB
+
+osdbcontrol -online ucos.db
