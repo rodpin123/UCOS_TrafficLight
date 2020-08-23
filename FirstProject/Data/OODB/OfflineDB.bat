4d7cebb7cd8e8e8b7d7a52eda843fb3256662bee
Rodrigo P <rodpin123@hotmail.com>
2020-08-22 22:51:49 -0300

    Need to update the correct files

--- FirstProject/Data/OODB/OfflineDB.bat
+++
@@ -1,4 +0,0 @@
-rem OfflineDB.bat 3-31-2006
-rem OfflineDB - PLACE IN C:\UCOS\UCOS_PDE_Projects\PROJNAME\Data\OODB
-
-osdbcontrol -offline ucos.db
--- FirstProject/Data/OODB/OnlineDB.bat
+++
@@ -1,4 +0,0 @@
-rem OnlineDB.bat 3-31-2006
-rem OnlineDB - PLACE IN C:\UCOS\UCOS_PDE_Projects\PROJNAME\Data\OODB
-
-osdbcontrol -online ucos.db
